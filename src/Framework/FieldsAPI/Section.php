a500f3fea8bec2afc622be4164527c9c00c125a7
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-05-09 16:27:58 -0400

    feature: create addFields method for more fluent Section type api

--- src/Framework/FieldsAPI/Section.php
+++ src/Framework/FieldsAPI/Section.php
@@ -11,4 +11,17 @@ class Section extends Group
      * @unreleased
      */
     const TYPE = 'section';
+
+    /**
+     * @unreleased
+     *
+     * @param  Field  ...$fields
+     * @return $this
+     */
+    public function addFields(Field ...$fields): Section
+    {
+        $this->append(...$fields);
+
+        return $this;
+    }
 }
