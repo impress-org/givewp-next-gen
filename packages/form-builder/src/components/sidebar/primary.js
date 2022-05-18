fcdbe5616db3062f10a3165ec6982ac242cff4ed
Kyle B. Johnson <kyle.johnson@impress.org>
2022-05-18 12:31:01 -0400

    wip: Add form title inspector control

--- packages/form-builder/src/components/sidebar/primary.js
+++ packages/form-builder/src/components/sidebar/primary.js
@@ -1,11 +1,50 @@
-import {createSlotFill, PanelHeader} from '@wordpress/components';
+import { useContext } from 'react'
+
+import {createSlotFill, TabPanel, PanelHeader, PanelBody, PanelRow, TextControl} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import {InspectorControls} from "@wordpress/block-editor";
+import {FormSettingsContext} from "../../context/formSettings";
 
 const { Slot: InspectorSlot, Fill: InspectorFill } = createSlotFill(
     'StandAloneBlockEditorSidebarInspector'
 );
 
+const tabs = [
+    {
+        name: 'form',
+        title: __('Form'),
+        className: 'tab-form',
+        content: ({ formSettings, setFormSettings }) => (
+            <PanelBody title={ __( 'Form Settings', 'give' ) } initialOpen={true}>
+                <PanelRow>
+                    <TextControl
+                        label={__('Form Title')}
+                        value={ formSettings.formTitle }
+                        onChange={ ( formTitle ) => {
+                            setFormSettings({
+                                ...formSettings,
+                                formTitle: formTitle,
+                            })
+                        } }
+                    />
+                </PanelRow>
+            </PanelBody>
+        )
+    },
+    {
+        name: 'block',
+        title: __('Block'),
+        className: 'tab-block',
+        content: () => (
+            <>
+                <InspectorSlot bubblesVirtually />
+            </>
+        )
+    },
+]
+
 function Sidebar() {
+    const [formSettings, setFormSettings] = useContext(FormSettingsContext)
     return (
         <div
             className="givewp-next-gen-sidebar givewp-next-gen-sidebar-primary"
@@ -13,8 +52,13 @@ function Sidebar() {
             aria-label={ __( 'Standalone Block Editor advanced settings.' ) }
             tabIndex="-1"
         >
-            <PanelHeader label={__('Settings')} />
-            <InspectorSlot bubblesVirtually />
+            <TabPanel
+                className="sidebar-panel"
+                activeClass="active-tab"
+                tabs={ tabs }
+            >
+                { ( tab ) => <tab.content formSettings={formSettings} setFormSettings={setFormSettings} /> }
+            </TabPanel>
         </div>
     );
 }
