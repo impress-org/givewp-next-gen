a192132a43bf27a68bc4efdd1a91862ad428b8a6
devinwalker <sdtucson@gmail.com>
2022-07-13 08:31:17 -0700

    Start adding better management of builder styles - point for discussion on stand up

--- packages/form-builder/src/App.js
+++ packages/form-builder/src/App.js
@@ -1,59 +1,50 @@
 import React, {useState} from 'react';
 
-import {parse} from '@wordpress/blocks'
+import {parse} from '@wordpress/blocks';
 import {ShortcutProvider} from '@wordpress/keyboard-shortcuts';
 import {BlockEditorProvider, BlockInspector} from '@wordpress/block-editor';
 import {SlotFillProvider, Popover} from '@wordpress/components';
-import {InterfaceSkeleton} from "@wordpress/interface";
+import {InterfaceSkeleton} from '@wordpress/interface';
 
-import Header from './components/header'
-import {Sidebar, SecondarySidebar} from './components/sidebar'
-import Content from './components/content'
+import Header from './components/header';
+import {Sidebar, SecondarySidebar} from './components/sidebar';
+import Content from './components/content';
 import {FormSettingsProvider, defaultFormSettings} from './settings/context';
 
-import {useToggleState} from "./hooks";
-
-import '@wordpress/components/build-style/style.css';
-import '@wordpress/block-editor/build-style/style.css';
+import {useToggleState} from './hooks';
 
 import './App.scss';
 
-import Storage from './components/storage'
-
+import Storage from './components/storage';
 
 function App() {
+    const {state: showSecondarySidebar, toggle: toggleSecondarySidebar} = useToggleState(false);
 
-    const {
-        state: showSecondarySidebar,
-        toggle: toggleSecondarySidebar
-    } = useToggleState(false)
-
-    const {
-        state: showSidebar,
-        toggle: toggleShowSidebar
-    } = useToggleState(true)
+    const {state: showSidebar, toggle: toggleShowSidebar} = useToggleState(true);
 
     const {blocks: initialBlocks, settings: initialFormSettings} = Storage.load();
     if (initialBlocks instanceof Error) {
-        alert('Unable to load initial blocks.')
+        alert('Unable to load initial blocks.');
         console.error(initialBlocks);
     }
 
     const [formSettings, setFormSettings] = useState({
         ...defaultFormSettings,
         ...initialFormSettings,
-    })
+    });
 
-    const [blocks, updateBlocks] = useState(initialBlocks || parse(`
+    const [blocks, updateBlocks] = useState(
+        initialBlocks ||
+            parse(`
         <!-- wp:custom-block-editor/donation-amount /-->
         <!-- wp:custom-block-editor/donor-info /-->
         <!-- wp:custom-block-editor/payment-details /-->
-    `));
+    `)
+    );
 
     const saveCallback = () => {
-        return Storage.save({blocks, formSettings})
-            .catch(error => alert(error.message));
-    }
+        return Storage.save({blocks, formSettings}).catch((error) => alert(error.message));
+    };
 
     return (
         <FormSettingsProvider formSettings={formSettings} setFormSettings={setFormSettings}>
@@ -65,21 +56,23 @@ function App() {
                 >
                     <SlotFillProvider>
                         <Sidebar.InspectorFill>
-                            <BlockInspector/>
+                            <BlockInspector />
                         </Sidebar.InspectorFill>
                         <InterfaceSkeleton
-                            header={<Header
-                                saveCallback={saveCallback}
-                                showSecondarySidebar={showSecondarySidebar}
-                                toggleSecondarySidebar={toggleSecondarySidebar}
-                                showSidebar={showSidebar}
-                                toggleShowSidebar={toggleShowSidebar}
-                            />}
-                            content={<Content/>}
-                            sidebar={!!showSidebar && <Sidebar/>}
-                            secondarySidebar={!!showSecondarySidebar && <SecondarySidebar/>}
+                            header={
+                                <Header
+                                    saveCallback={saveCallback}
+                                    showSecondarySidebar={showSecondarySidebar}
+                                    toggleSecondarySidebar={toggleSecondarySidebar}
+                                    showSidebar={showSidebar}
+                                    toggleShowSidebar={toggleShowSidebar}
+                                />
+                            }
+                            content={<Content />}
+                            sidebar={!!showSidebar && <Sidebar />}
+                            secondarySidebar={!!showSecondarySidebar && <SecondarySidebar />}
                         />
-                        <Popover.Slot/>
+                        <Popover.Slot />
                     </SlotFillProvider>
                 </BlockEditorProvider>
             </ShortcutProvider>
