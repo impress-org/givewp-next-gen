e52048699ec8da04c3ecf29e57caaebaabe4701f
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-07-13 10:29:57 -0400

    chore: cleanup bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,11 +10,3 @@ tests_add_filter('muplugins_loaded', function () {
 });
 
 require_once __DIR__ . '/../../give/tests/unit/bootstrap.php';
-
-//tests_add_filter('muplugins_loaded', static function() {
-//    require_once __DIR__ . "/../../give/give.php";
-//});
-
-//require_once __DIR__ . "/../give-next-gen.php";
-//require_once __DIR__ . "/../vendor/autoload.php";
-//require_once __DIR__ . "/../../../../wp-load.php";
