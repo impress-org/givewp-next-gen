2cccdf2c31df58bd4b3bc6a1e339d289ae318ce3
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-07-14 17:14:25 -0400

    refactor: break out Header component into presentational and container.  organize folder structure a little more

--- packages/form-builder/src/components/icons/index.js
+++ packages/form-builder/src/components/icons/index.js
@@ -1,7 +1,3 @@
-import ListIcon from './list'
-import SettingsIcon from './settings'
-
-export {
-    ListIcon,
-    SettingsIcon
-}
+export {default as ListIcon} from './list';
+export {default as SettingsIcon} from './settings';
+export {default as GiveIcon} from './logo';
