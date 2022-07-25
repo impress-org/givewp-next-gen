da93716cd853e7f6ec646b89a7c82ad50c68b308
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-07-25 16:59:06 -0400

    refactor: convert amount field to BEM structure

--- src/NextGen/DonationForm/FormTemplates/ClassicFormTemplate/js/fields/AmountField.jsx
+++ src/NextGen/DonationForm/FormTemplates/ClassicFormTemplate/js/fields/AmountField.jsx
@@ -17,31 +17,32 @@ export default function AmountField({name, label, inputProps, levels, allowCusto
     );
 
     return (
-        <div className="givewp-fields-amount-container">
-            <div>
-                <label htmlFor={name} aria-labelledby={name} style={{display: 'none'}}>
+        <>
+            <div className="givewp-fields-amount__amount--container">
+                <label className="givewp-fields-amount__input--label" htmlFor={name} aria-labelledby={name}
+                       style={{display: 'none'}}>
                     {label ?? __('Donation Amount', 'give')}
                 </label>
-                <div className={classNames("givewp-amount-input-container", {'invalid': fieldError})}>
-                    <span
-                        className="givewp-currency-symbol">{formatter.formatToParts().find(({type}) => type === 'currency').value}</span>
-                    <input className='givewp-amount-input' type={allowCustomAmount ? 'text' : 'hidden'}
+                <div className={classNames("givewp-fields-amount__input--container", {'invalid': fieldError})}>
+                <span
+                    className="givewp-fields-amount__input--currency-symbol">{formatter.formatToParts().find(({type}) => type === 'currency').value}</span>
+                    <input className='givewp-fields-amount__input' type={allowCustomAmount ? 'text' : 'hidden'}
                            aria-invalid={fieldError ? "true" : "false"}
                            id={name}
                            inputMode="numeric" {...inputProps} />
                 </div>
 
-                <div className="error-message">
+                <div className="givewp-fields-amount__error-message error-message">
                     {fieldError && <p role="alert">{fieldError}</p>}
                 </div>
             </div>
-
-            <div className="givewp-amount-levels-container">
+            <div className="givewp-fields-amount__levels--container">
                 {levels.map((levelAmount) => {
                     const label = formatter.format(levelAmount);
                     const selected = levelAmount === Number(amount);
                     return (
-                        <button className={classNames('givewp-amount-level', {'selected': selected})} type="button"
+                        <button className={classNames('givewp-fields-amount__level', {'selected': selected})}
+                                type="button"
                                 onClick={() => setValue(name, levelAmount)} key={label}>
                             {label}
                         </button>
@@ -49,21 +50,19 @@ export default function AmountField({name, label, inputProps, levels, allowCusto
                 })}
 
                 <button
-                    className={
-                        classNames(
-                            'givewp-amount-level',
-                            'givewp-amount-level-custom',
-                            {'selected': !levels.includes(Number(amount))}
-                        )
-                    }
+                    className={classNames(
+                        'givewp-fields-amount__level',
+                        'givewp-fields-amount__level--custom',
+                        {'selected': !levels.includes(Number(amount))}
+                    )}
                     type="button"
                     onClick={() => {
                         setValue(name, null);
-                        setFocus("amount", {shouldSelect: true})
+                        setFocus("amount", {shouldSelect: true});
                     }} key='custom'>
                     {__('Custom Amount', 'give')}
                 </button>
             </div>
-        </div>
+        </>
     );
 }
