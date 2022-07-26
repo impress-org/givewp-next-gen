c8a70de4cb054364a64464f8aa2afbf57bae32c5
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-07-26 15:03:08 -0400

    chore: format file, c'mon phpstorm

--- src/NextGen/DonationForm/Blocks/DonationFormBlock/app/fields/GroupNode.tsx
+++ src/NextGen/DonationForm/Blocks/DonationFormBlock/app/fields/GroupNode.tsx
@@ -5,21 +5,20 @@ import {useFormContext} from "react-hook-form";
 import {useMemo} from "react";
 import getGroupFields from "../utilities/getGroupFields";
 
-export default function GroupNode({node}: {node: Group}) {
+export default function GroupNode({node}: { node: Group }) {
     const {register} = useFormContext();
-
     const Group = useMemo(() => getGroupTemplate(node.type), [node.type]);
-        const fields = node.nodes.reduce(getGroupFields, []);
+    const fields = node.nodes.reduce(getGroupFields, []);
 
-        const inputProps = fields.reduce((inputProps, field) => {
-            inputProps[field.name] = register(
-                field.name,
-                buildRegisterValidationOptions(field.validationRules)
-            );
+    const inputProps = fields.reduce((inputProps, field) => {
+        inputProps[field.name] = register(
+            field.name,
+            buildRegisterValidationOptions(field.validationRules)
+        );
 
-            return inputProps;
-        }, {});
+        return inputProps;
+    }, {});
 
-        return <Group key={node.name} inputProps={inputProps} {...node} />;
+    return <Group key={node.name} inputProps={inputProps} {...node} />;
 }
 
