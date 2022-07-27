9ff30a146c98d3bf6bc4d80910d15542df3f4130
Kyle B. Johnson <kyle.johnson@impress.org>
2022-07-27 15:43:30 -0400

    refactor: Update fork with source component

--- packages/form-builder/src/components/sidebar/tab-panel.js
+++ packages/form-builder/src/components/sidebar/tab-panel.js
@@ -5,70 +5,55 @@
  *  - Replace useState hook with injected state for external control of the selected tab.
  *
  * Maintenance changes:
+ *  - Removed import of useState.
  *  - Update relative component imports for NavigableMenu, Button.
  *  - Disabled eslint rule react-hooks/exhaustive-deps for useEffect in the TabPanel component.
  */
-
-import _extends from "@babel/runtime/helpers/esm/extends";
-import {createElement} from "@wordpress/element";
 
 /**
  * External dependencies
  */
 import classnames from 'classnames';
-import {partial, find} from 'lodash';
+import {partial, noop, find} from 'lodash';
+
 /**
  * WordPress dependencies
  */
-
 import {/* useState, */ useEffect} from '@wordpress/element';
 import {useInstanceId} from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
-
 import {NavigableMenu, Button} from '@wordpress/components';
-// import { NavigableMenu } from '../navigable-container';
-// import Button from '../button';
 
-const noop = () => {
-};
+const TabButton = ({tabId, onClick, children, selected, ...rest}) => (
+    <Button
+        role="tab"
+        tabIndex={selected ? null : -1}
+        aria-selected={selected}
+        id={tabId}
+        onClick={onClick}
+        {...rest}
+    >
+        {children}
+    </Button>
+);
 
-const TabButton = _ref => {
-    let {
-        tabId,
-        onClick,
-        children,
-        selected,
-        ...rest
-    } = _ref;
-    return createElement(Button, _extends({
-        role: "tab",
-        tabIndex: selected ? null : -1,
-        "aria-selected": selected,
-        id: tabId,
-        onClick: onClick,
-    }, rest), children);
-};
-
-export default function TabPanel(_ref2) {
-    var _selectedTab$name;
-
-    let {
-        className,
-        children,
-        tabs,
-        initialTabName,
-        orientation = 'horizontal',
-        activeClass = 'is-active',
-        onSelect = noop,
-        state: [selected, setSelected], /** @note Injecting state for external control of the selected tab. */
-    } = _ref2;
+export default function TabPanel({
+                                     className,
+                                     children,
+                                     tabs,
+                                     initialTabName,
+                                     orientation = 'horizontal',
+                                     activeClass = 'is-active',
+                                     onSelect = noop,
+                                     state: [selected, setSelected], /** @note Injecting state for external control of the selected tab. */
+                                 }) {
     const instanceId = useInstanceId(TabPanel, 'tab-panel');
-    // const [selected, setSelected] = useState(null);
+    // const [selected, setSelected] = useState(null); /** @note Replaced with injected state. */
 
-    const handleClick = tabKey => {
+    const handleClick = (tabKey) => {
         setSelected(tabKey);
         onSelect(tabKey);
     };
@@ -76,42 +61,56 @@ export default function TabPanel(_ref2) {
     const onNavigate = (childIndex, child) => {
         child.click();
     };
+    const selectedTab = find(tabs, {name: selected});
+    const selectedId = `${instanceId}-${selectedTab?.name ?? 'none'}`;
 
-    const selectedTab = find(tabs, {
-        name: selected,
-    });
-    const selectedId = `${instanceId}-${(_selectedTab$name = selectedTab === null || selectedTab === void 0 ? void 0 : selectedTab.name) !== null && _selectedTab$name !== void 0 ? _selectedTab$name : 'none'}`;
     useEffect(() => {
-        const newSelectedTab = find(tabs, {
-            name: selected,
-        });
-
+        const newSelectedTab = find(tabs, {name: selected});
         if (!newSelectedTab) {
-            setSelected(initialTabName || (tabs.length > 0 ? tabs[0].name : null));
+            setSelected(
+                initialTabName || (tabs.length > 0 ? tabs[0].name : null),
+            );
         }
     }, [tabs]); // eslint-disable-line react-hooks/exhaustive-deps
-    return createElement("div", {
-        className: className,
-    }, createElement(NavigableMenu, {
-        role: "tablist",
-        orientation: orientation,
-        onNavigate: onNavigate,
-        className: "components-tab-panel__tabs",
-    }, tabs.map(tab => createElement(TabButton, {
-        className: classnames('components-tab-panel__tabs-item', tab.className, {
-            [activeClass]: tab.name === selected,
-        }),
-        tabId: `${instanceId}-${tab.name}`,
-        "aria-controls": `${instanceId}-${tab.name}-view`,
-        selected: tab.name === selected,
-        key: tab.name,
-        onClick: partial(handleClick, tab.name),
-    }, tab.title))), selectedTab && createElement("div", {
-        key: selectedId,
-        "aria-labelledby": selectedId,
-        role: "tabpanel",
-        id: `${selectedId}-view`,
-        className: "components-tab-panel__tab-content",
-    }, children(selectedTab)));
+
+    return (
+        <div className={className}>
+            <NavigableMenu
+                role="tablist"
+                orientation={orientation}
+                onNavigate={onNavigate}
+                className="components-tab-panel__tabs"
+            >
+                {tabs.map((tab) => (
+                    <TabButton
+                        className={classnames(
+                            'components-tab-panel__tabs-item',
+                            tab.className,
+                            {
+                                [activeClass]: tab.name === selected,
+                            },
+                        )}
+                        tabId={`${instanceId}-${tab.name}`}
+                        aria-controls={`${instanceId}-${tab.name}-view`}
+                        selected={tab.name === selected}
+                        key={tab.name}
+                        onClick={partial(handleClick, tab.name)}
+                    >
+                        {tab.title}
+                    </TabButton>
+                ))}
+            </NavigableMenu>
+            {selectedTab && (
+                <div
+                    key={selectedId}
+                    aria-labelledby={selectedId}
+                    role="tabpanel"
+                    id={`${selectedId}-view`}
+                    className="components-tab-panel__tab-content"
+                >
+                    {children(selectedTab)}
+                </div>
+            )}
+        </div>
+    );
 }
-//# sourceMappingURL=index.js.map
