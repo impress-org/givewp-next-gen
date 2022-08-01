bf9172d027ffe5d033584f5a6807f1b6a15c2eca
Kyle B. Johnson <kyle.johnson@impress.org>
2022-08-01 15:29:32 -0400

    feature: Add reactive amount to donation summary

--- src/NextGen/DonationForm/Blocks/DonationFormBlock/app/templates/elements/DonationSummary.tsx
+++ src/NextGen/DonationForm/Blocks/DonationFormBlock/app/templates/elements/DonationSummary.tsx
@@ -1,16 +1,29 @@
-import type {ElementProps} from '@givewp/forms/propTypes';
+import {useMemo} from "react";
 
 export default function DonationSummary() {
+
+    const {useWatch} = window.givewp.form;
+    const currency = useWatch({name: 'currency'});
+    const amount = useWatch({name: 'amount'});
+    const formatter = useMemo(
+        () =>
+            new Intl.NumberFormat(navigator.language, {
+                style: 'currency',
+                currency: currency,
+            }),
+        [currency, navigator.language]
+    )
+
     return <div>
         <ul style={{listStyleType: 'none'}}>
-            <LineItem label={'Payment Amount'} />
-            <LineItem label={'Giving Frequency'} />
-            <LineItem label={'Donation Total'} />
+            <LineItem label={'Payment Amount'} value={formatter.format(Number(amount))} />
+            <LineItem label={'Giving Frequency'} value={'One time'} />
+            <LineItem label={'Donation Total'} value={formatter.format(Number(amount))} />
         </ul>
     </div>;
 }
 
-const LineItem = ({label}) => {
+const LineItem = ({label, value}) => {
     return (
         <li style={{
             display: 'flex',
@@ -19,7 +32,7 @@ const LineItem = ({label}) => {
             borderBlockStart: '0.0625rem solid #ddd',
         }}>
             <div>{label}</div>
-            <div>-</div>
+            <div>{value}</div>
         </li>
     )
 }
