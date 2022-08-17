6ff3cc3888c670f1dd316fc787b33091fe817681
Kyle B. Johnson <kyle.johnson@impress.org>
2022-08-17 15:37:52 -0400

    refactor: Update name field layout

--- packages/form-builder/src/blocks/fields/donor-name.js
+++ packages/form-builder/src/blocks/fields/donor-name.js
@@ -66,7 +66,11 @@ const donorName = {
 
             return (
                 <>
-                    <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr 2fr', gap: '15px'}}>
+                    <div style={{
+                        display: 'grid',
+                        gridTemplateColumns: showHonorific ? '1fr 2fr 2fr' : '1fr 1fr',
+                        gap: '15px',
+                    }}>
                         {!!showHonorific && <HonorificSelect honorifics={honorifics} />}
                         <TextControl label={firstNameLabel} placeholder={firstNamePlaceholder} required={true}
                                      className={'give-is-required'} />
