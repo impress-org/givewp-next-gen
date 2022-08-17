14ad5e38f62651a13da3ca52d733c8be4be939c0
Kyle B. Johnson <kyle.johnson@impress.org>
2022-08-17 15:38:19 -0400

    fix: typo

--- packages/form-builder/src/blocks/fields/donor-name.js
+++ packages/form-builder/src/blocks/fields/donor-name.js
@@ -37,7 +37,7 @@ const donorName = {
             lastNameLabel: {
                 type: 'string',
                 source: 'attribute',
-                default: __('First name', 'give'),
+                default: __('Last name', 'give'),
             },
             lastNamePlaceholder: {
                 type: 'string',
