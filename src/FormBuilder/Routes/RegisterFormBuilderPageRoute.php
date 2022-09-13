fcbae2f920e72e5786e669f32fee1227efc8a52b
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-09-13 16:10:10 -0400

    refactor: use GiveWP EnqueueScript helper and cleanup localized data

--- src/FormBuilder/Routes/RegisterFormBuilderPageRoute.php
+++ src/FormBuilder/Routes/RegisterFormBuilderPageRoute.php
@@ -2,13 +2,16 @@
 
 namespace Give\FormBuilder\Routes;
 use Give\Addon\View;
+use Give\Framework\EnqueueScript;
 
 /**
- * Add form builder page routes with scripts
+ * Add form builder page route and scripts
  */
 class RegisterFormBuilderPageRoute
 {
     /**
+     * Use add_submenu_page to register page within WP admin
+     *
      * @unreleased
      *
      * @return void
@@ -21,48 +24,74 @@ class RegisterFormBuilderPageRoute
             __('Visual Builder <span class="awaiting-mod">Alpha</span>', 'givewp'),
             'manage_options',
             'campaign-builder',
-            function () {
-                $manifest = json_decode(
-                    file_get_contents(GIVE_NEXT_GEN_DIR . 'packages/form-builder/build/asset-manifest.json')
-                );
-                [$css, $js] = $manifest->entrypoints;
+            [$this, 'renderPage'],
+            1
+        );
+    }
 
-                View::render('FormBuilder.admin-form-builder', [
-                    'shadowDomStyles' => file_get_contents(
-                        trailingslashit(GIVE_NEXT_GEN_DIR) . 'packages/form-builder/build/' . $css
-                    ),
-                ]);
+    /**
+     * Render page with scripts
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    public function renderPage()
+    {
+        [$css, $js] = $this->getAssetManifest();
 
-                wp_enqueue_script(
-                    '@givewp/form-builder/storage',
-                    trailingslashit(GIVE_NEXT_GEN_URL) . 'src/FormBuilder/resources/js/storage.js'
-                );
-                wp_localize_script('@givewp/form-builder/storage', 'giveCurrency', [
-                    'currency' => give_get_currency(),
-                ]);
-                wp_localize_script('@givewp/form-builder/storage', 'storageData', [
-                    'resourceURL' => rest_url('givewp/next-gen/form/' . abs($_GET['donationFormID'])),
-                    'nonce' => wp_create_nonce('wp_rest'),
-                    'blockData' => get_post(abs($_GET['donationFormID']))->post_content,
-                    'settings' => get_post_meta(abs($_GET['donationFormID']), 'formBuilderSettings', true),
-                ]);
+        $formBuilderStorage = (new EnqueueScript(
+            '@givewp/form-builder/storage',
+            'src/FormBuilder/resources/js/storage.js',
+            GIVE_NEXT_GEN_DIR,
+            GIVE_NEXT_GEN_URL,
+            'give'
+        ));
 
-                wp_enqueue_script(
-                    '@givewp/form-builder/script',
-                    trailingslashit(GIVE_NEXT_GEN_URL) . 'packages/form-builder/build/' . $js, [],
-                    false,
-                    true
-                );
-                wp_add_inline_script(
-                    '@givewp/form-builder/script',
-                    "
+        $formBuilderStorage->registerLocalizeData('storageData', [
+            'resourceURL' => rest_url('givewp/next-gen/form/' . abs($_GET['donationFormID'])),
+            'nonce' => wp_create_nonce('wp_rest'),
+            'blockData' => get_post(abs($_GET['donationFormID']))->post_content,
+            'settings' => get_post_meta(abs($_GET['donationFormID']), 'formBuilderSettings', true),
+            'currency' => give_get_currency(),
+        ]);
+
+        $formBuilderStorage->loadInFooter()->enqueue();
+
+        (new EnqueueScript(
+            '@givewp/form-builder/script',
+            'packages/form-builder/build/' . $js,
+            GIVE_NEXT_GEN_DIR,
+            GIVE_NEXT_GEN_URL,
+            'give'
+        ))->loadInFooter()->enqueue();
+
+        wp_add_inline_script(
+            '@givewp/form-builder/script',
+            "
                         document.getElementById('app').attachShadow({mode: 'open'})
                             .appendChild( document.getElementById('root') )
                             .appendChild( document.getElementById('shadowDomStyles') )
                     "
-                );
-            },
-            1
         );
+
+        View::render('FormBuilder.admin-form-builder', [
+            'shadowDomStyles' => file_get_contents(
+                trailingslashit(GIVE_NEXT_GEN_DIR) . 'packages/form-builder/build/' . $css
+            ),
+        ]);
+    }
+
+    /**
+     * Get css and js paths from manifest file
+     *
+     * @unreleased
+     */
+    public function getAssetManifest(): array
+    {
+        return json_decode(
+            file_get_contents(GIVE_NEXT_GEN_DIR . 'packages/form-builder/build/asset-manifest.json'),
+            false
+        )->entrypoints;
     }
 }
