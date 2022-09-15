9dac5161d95271c72977ec063552a5f83070ccfc
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-09-15 16:38:33 -0400

    refactor: simplify config without enu

--- src/FormBuilder/ValueObjects/FormBuilderRestRouteConfig.php
+++ src/FormBuilder/ValueObjects/FormBuilderRestRouteConfig.php
@@ -2,15 +2,10 @@
 
 namespace Give\FormBuilder\ValueObjects;
 
-use Give\Framework\Support\ValueObjects\Enum;
-
 /**
  * @unreleased
- *
- * @method static FormBuilderRestRouteConfig NAMESPACE()
- * @method static FormBuilderRestRouteConfig PATH()
  */
-class FormBuilderRestRouteConfig extends Enum
+class FormBuilderRestRouteConfig
 {
     const NAMESPACE = 'givewp/next-gen';
     const PATH = '/form/(?P<id>\d+)';
