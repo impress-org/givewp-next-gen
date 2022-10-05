e3d3b0dc258e14eb11f715ed2bef98072892bdb7
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-10-05 13:16:01 -0400

    fix: text field should spread validation rules

--- src/NextGen/DonationForm/Blocks/DonationFormBlock/resources/app/templates/fields/Text.tsx
+++ src/NextGen/DonationForm/Blocks/DonationFormBlock/resources/app/templates/fields/Text.tsx
@@ -1,10 +1,16 @@
 import type {FieldProps} from '@givewp/forms/propTypes';
 
-export default function Text({label, placeholder, fieldError, inputProps}: FieldProps) {
+export default function Text({label, placeholder, fieldError, inputProps, validationRules}: FieldProps) {
     return (
         <label>
             <span>{label}</span>
-            <input type="text" aria-invalid={fieldError ? 'true' : 'false'} placeholder={placeholder} {...inputProps} />
+            <input
+                type="text"
+                aria-invalid={fieldError ? 'true' : 'false'}
+                placeholder={placeholder}
+                {...inputProps}
+                {...validationRules}
+            />
 
             {fieldError && (
                 <div className="error-message">
