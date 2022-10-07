1946b45847abea10f2a38794d7a7be786094d724
Jason Adams <jason.adams@impress.org>
2022-10-07 16:54:01 -0700

    refactor: consolidate functions for getting group fields

--- src/NextGen/DonationForm/Blocks/DonationFormBlock/resources/app/fields/GroupNode.tsx
+++ src/NextGen/DonationForm/Blocks/DonationFormBlock/resources/app/fields/GroupNode.tsx
@@ -3,13 +3,13 @@ import {Group} from '@givewp/forms/types';
 import {getGroupTemplate} from '../templates';
 import {useFormContext} from 'react-hook-form';
 import {useMemo} from 'react';
-import getGroupFields from '../utilities/getGroupFields';
 import addRequiredToFieldLabel from '../utilities/addRequiredToFieldLabel';
+import {getGroupFields} from '../utilities/groups';
 
 export default function GroupNode({node}: {node: Group}) {
     const {register} = useFormContext();
     const Group = useMemo(() => getGroupTemplate(node.type), [node.type]);
-    const fields = node.nodes.reduce(getGroupFields, []);
+    const fields = getGroupFields(node);
 
     fields.forEach((field) => {
         addRequiredToFieldLabel(field);
