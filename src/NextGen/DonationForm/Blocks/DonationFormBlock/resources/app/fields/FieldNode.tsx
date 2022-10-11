f023a823900ba03f1ff6086817e994a151e527d0
Jason Adams <jason.adams@impress.org>
2022-10-10 17:42:07 -0700

    chore: remove commented code

--- src/NextGen/DonationForm/Blocks/DonationFormBlock/resources/app/fields/FieldNode.tsx
+++ src/NextGen/DonationForm/Blocks/DonationFormBlock/resources/app/fields/FieldNode.tsx
@@ -4,18 +4,12 @@ import {getFieldTemplate} from '../templates';
 import getErrorByFieldName from '../utilities/getErrorByFieldName';
 import {useFormContext, useFormState} from 'react-hook-form';
 import {useMemo} from 'react';
-import generateRequiredLabel from '../utilities/generateRequiredLabel';
 
 export default function FieldNode({node}: {node: Field}) {
     const {register} = useFormContext();
     const {errors} = useFormState();
     const Field = useMemo(() => getFieldTemplate(node.type), [node.type]);
     const inputProps = register(node.name, buildRegisterValidationOptions(node.validationRules));
-
-    // node.requiredLabel = useMemo(
-    //     () => generateRequiredLabel(node.label, node.validationRules.required),
-    //     [node.label, node.validationRules.required]
-    // );
 
     return (
         <Field
