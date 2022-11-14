83315a61a75acede597b1e76d81f1697a2de6256
Kyle B. Johnson <kyle.johnson@hey.com>
2022-11-14 14:26:31 -0500

    refactor: Update iframe reloading

--- packages/form-builder/src/components/canvas/DesignPreview.tsx
+++ packages/form-builder/src/components/canvas/DesignPreview.tsx
@@ -1,41 +1,53 @@
 import * as React from 'react';
 import {useEffect, useState} from 'react';
-import DesignPreviewLoading from './DesignPreviewLoading';
 
 import Storage from '@givewp/form-builder/common/storage';
 
 import IframeResizer from 'iframe-resizer-react';
 import {useFormState} from '../../stores/form-state';
+import DesignPreviewLoading from "@givewp/form-builder/components/canvas/DesignPreviewLoading";
 
 const DesignPreview = () => {
     const {blocks, settings: formSettings} = useFormState();
-    const [sourceDocument, setSourceDocument] = useState('');
-    const [isLoading, setIsLoading] = useState<boolean>();
+    const [isLoading, setLoading] = useState(true);
+    const [sourceDocument, setSourceDocument] = useState(null);
+    const [previousPreviewHTML, setPreviousPreviewHTML] = useState(null);
 
     useEffect(() => {
-        setIsLoading(true);
+        setLoading(true);
         Storage.preview({blocks, formSettings}).then((document) => {
             setSourceDocument(document);
-            setIsLoading(false);
         });
     }, [
         JSON.stringify(formSettings),
         JSON.stringify(blocks), // stringify to prevent re-renders caused by object as dep
     ]);
 
-    return isLoading ? (
-        <DesignPreviewLoading />
-    ) : (
-        <IframeResizer
-            srcDoc={sourceDocument}
-            checkOrigin={false} /** The srcDoc property is not a URL and requires that the origin check be disabled. */
-            style={{
-                width: '1px',
-                minWidth: '100%',
-                border: '0',
-            }}
-        />
-    );
+    const iframeStyles = {
+        width: '1px',
+        minWidth: '100%',
+        border: '0',
+    }
+
+    return (
+        <>
+            {isLoading && <DesignPreviewLoading />}
+            <IframeResizer
+                onLoad={(event) => {
+                    const target = event.target as HTMLIFrameElement;
+                    setPreviousPreviewHTML(target.contentWindow.document.documentElement.innerHTML)
+                    setLoading(false)
+                }}
+                srcDoc={sourceDocument}
+                style={{display: 'none'}}
+            />
+            <IframeResizer
+                srcDoc={previousPreviewHTML}
+                checkOrigin={false} /** The srcDoc property is not a URL and requires that the origin check be disabled. */
+                style={iframeStyles}
+            />
+        </>
+    )
 };
 
 export default DesignPreview;
