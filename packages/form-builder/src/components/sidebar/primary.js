babc7585f2046a75d9433d3e5f69efe05d1bf231
Kyle B. Johnson <kyle.johnson@hey.com>
2022-11-16 12:07:31 -0600

    chore: Restore Donation Goal to form settings

--- packages/form-builder/src/components/sidebar/primary.js
+++ packages/form-builder/src/components/sidebar/primary.js
@@ -26,6 +26,7 @@ const tabs = [
         content: () => (
             <>
                 <FormTitleSettings />
+                <DonationGoalSettings />
                 <OfflineDonationsSettings />
                 <FormFields />
             </>
