952f11b74c07f28f71d3cbfc847cdd406c51ebea
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2022-11-22 14:55:06 -0500

    refactor: remove temporary hidden formId casting to string

--- src/NextGen/DonationForm/Repositories/DonationFormRepository.php
+++ src/NextGen/DonationForm/Repositories/DonationFormRepository.php
@@ -357,8 +357,7 @@ class DonationFormRepository
         if ($lastSection) {
             $lastSection->append(
                 Hidden::make('formId')
-                    // Casting this value to a string is a temporary solution for front-end validation until we have proper validation rule constructing in the Fields API.
-                    ->defaultValue((string)$formId)
+                    ->defaultValue($formId)
                     ->rules('required', 'integer'),
 
                 Hidden::make('currency')
