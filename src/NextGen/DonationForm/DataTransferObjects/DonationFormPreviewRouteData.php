8184e7037c122bfab6d45d16c4f101f25931b95c
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2022-11-22 16:42:08 -0500

    feature: add preview form view route, seperate view logic, consolidate render into viewModel, add and update unit tests

---
+++ src/NextGen/DonationForm/DataTransferObjects/DonationFormPreviewRouteData.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Give\NextGen\DonationForm\DataTransferObjects;
+
+use Give\NextGen\Framework\Blocks\BlockCollection;
+
+/**
+ * @unreleased
+ */
+class DonationFormPreviewRouteData
+{
+    /**
+     * @var int
+     */
+    public $formId;
+
+    /**
+     * @var BlockCollection|null
+     */
+    public $formBlocks;
+
+    /**
+     * @var array
+     */
+    public $formSettings;
+
+    /**
+     * Convert data from request into DTO
+     *
+     * @param  array{form-id: string, form-settings: string, form-blocks: string}  $request
+     *
+     * @unreleased
+     */
+    public static function fromRequest(array $request): self
+    {
+        $self = new static();
+
+        $self->formId = (int)$request['form-id'];
+        $self->formSettings = !empty($request['form-settings']) ? json_decode($request['form-settings'], true) : [];
+        $self->formBlocks = !empty($request['form-blocks']) ? BlockCollection::fromJson(
+            $request['form-blocks']
+        ) : null;
+
+        return $self;
+    }
+}
