a6662eedcc863cde830ea7e3f3089bb35d5e73bf
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2022-12-16 17:32:10 -0500

    chore: cleanup unused

--- src/NextGen/DonationForm/DataTransferObjects/DonateFormRouteData.php
+++ src/NextGen/DonationForm/DataTransferObjects/DonateFormRouteData.php
@@ -3,7 +3,6 @@
 namespace Give\NextGen\DonationForm\DataTransferObjects;
 
 use Give\Framework\FieldsAPI\Actions\CreateValidatorFromForm;
-use Give\Framework\Validation\Validator;
 use Give\NextGen\DonationForm\Exceptions\DonationFormFieldErrorsException;
 use Give\NextGen\DonationForm\Models\DonationForm;
 use WP_Error;
