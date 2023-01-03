41dae79f06fed49d38f6f87b861c7a37f9d9e2af
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2023-01-03 14:44:38 -0500

    chore: Update menu title

--- src/FormBuilder/Routes/RegisterFormBuilderPageRoute.php
+++ src/FormBuilder/Routes/RegisterFormBuilderPageRoute.php
@@ -21,7 +21,7 @@ class RegisterFormBuilderPageRoute
     public function __invoke()
     {
         $pageTitle = __('Visual Donation Form Builder', 'givewp');
-        $menuTitle = __('Add Form', 'givewp');
+        $menuTitle = __('Add Next Gen Form', 'givewp');
         $version = __('Beta', 'givewp');
 
         add_submenu_page(
