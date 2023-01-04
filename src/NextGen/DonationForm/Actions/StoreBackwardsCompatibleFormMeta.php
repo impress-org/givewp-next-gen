53853b0162b08281d1f2572b55336511b38bfc30
Kyle B. Johnson <kyle.johnson@hey.com>
2023-01-04 14:59:43 -0500

    fix: Store backwards compatible form meta

---
+++ src/NextGen/DonationForm/Actions/StoreBackwardsCompatibleFormMeta.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Give\NextGen\DonationForm\Actions;
+
+use Give\DonationForms\ValueObjects\DonationFormMetaKeys;
+use Give\NextGen\DonationForm\Models\DonationForm;
+
+class StoreBackwardsCompatibleFormMeta
+{
+    protected function storeDonationLevels(DonationForm $donationForm)
+    {
+        $donationLevels = $donationForm->schema()->getNodeByName('amount')->getLevels();
+        $donationLevels = array_map(function ($donationLevel, $index) {
+            return [
+                '_give_id' => [
+                    'level_id' => $index,
+                ],
+                '_give_amount' => $donationLevel
+            ];
+        }, $donationLevels, array_keys($donationLevels));
+
+        give()->form_meta->update_meta($donationForm->id, DonationFormMetaKeys::DONATION_LEVELS, $donationLevels);
+    }
+
+    protected function storeDonationGoal(DonationForm $donationForm)
+    {
+        give()->form_meta->update_meta($donationForm->id, DonationFormMetaKeys::GOAL_OPTION, $donationForm->settings->enableDonationGoal ? 'enabled' : 'disabled');
+
+        $goalType = $donationForm->settings->goalType->getValue();
+        $goalType = ($goalType === 'donations') ? 'donation' : $goalType; // @todo Mismatch. Legacy uses "donation" instead of "donations".
+        give()->form_meta->update_meta($donationForm->id, '_give_goal_format', $goalType);
+
+        $metaLookup = [
+            'donation' => '_give_number_of_donation_goal',
+            'donors' => '_give_number_of_donor_goal',
+            'amount' => '_give_set_goal',
+        ];
+
+        $goalAmount = ('amount' === $goalType) ? give_sanitize_amount_for_db($donationForm->settings->goalAmount) : $donationForm->settings->goalAmount;
+        if( give()->form_meta->get_meta($donationForm->id, $metaLookup[$goalType], true)) {
+            give()->form_meta->update_meta($donationForm->id, $metaLookup[$goalType], $goalAmount);
+        } else {
+            give()->form_meta->add_meta($donationForm->id, $metaLookup[$goalType], $goalAmount);
+        }
+    }
+}
