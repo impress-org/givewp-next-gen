0eb9708ae44d8824d843844be8fe85ccc51e5d97
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2023-01-11 11:40:10 -0500

    refactor: use local query for now

--- src/NextGen/DonationForm/Controllers/DonationConfirmationReceiptViewController.php
+++ src/NextGen/DonationForm/Controllers/DonationConfirmationReceiptViewController.php
@@ -2,6 +2,9 @@
 
 namespace Give\NextGen\DonationForm\Controllers;
 
+use Give\Donations\Models\Donation;
+use Give\Donations\ValueObjects\DonationMetaKeys;
+use Give\Framework\QueryBuilder\QueryBuilder;
 use Give\NextGen\DonationForm\DataTransferObjects\DonationConfirmationReceiptViewRouteData;
 use Give\NextGen\DonationForm\ViewModels\DonationConfirmationReceiptViewModel;
 
@@ -14,12 +17,30 @@ class DonationConfirmationReceiptViewController
      */
     public function show(DonationConfirmationReceiptViewRouteData $data): string
     {
-        $donation = give()->donations->getByPurchaseKey($data->receiptId);
+        $donation = $this->getDonationByReceiptId($data->receiptId);
 
         if (!$donation) {
             return '';
         }
 
         return (new DonationConfirmationReceiptViewModel($donation))->render();
+    }
+
+    /**
+     * @unreleased
+     *
+     * @return Donation|null
+     */
+    private function getDonationByReceiptId(string $receiptId)
+    {
+        return give()->donations->prepareQuery()
+            ->where('post_type', 'give_payment')
+            ->where('ID', function (QueryBuilder $builder) use ($receiptId) {
+                $builder
+                    ->select('donation_id')
+                    ->from('give_donationmeta')
+                    ->where('meta_key', DonationMetaKeys::PURCHASE_KEY()->getValue())
+                    ->where('meta_value', $receiptId);
+            })->get();
     }
 }
