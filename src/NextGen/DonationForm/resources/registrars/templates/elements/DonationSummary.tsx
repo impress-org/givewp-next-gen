dc838b0d19653418e1bfee0d587e5ba31e703a95
Jon Waldstein <Jpwaldstein@gmail.com>
2023-02-09 17:14:54 -0500

    Feature: Amount field template updates (#126)
    * feature: add type and customAmountText to amount field
    
    * feature: scaffold DonationAmount group using amount field and hidden period, frequency, times fields
    
    * feature: add allowLevels prop
    
    * feature: add amount group fields for donation type and currency
    
    * feature: add donationType and currency to DonationAmount group
    
    * feature: add custom validation to amount field specific fields
    
    * refactor: use DonationType enum
    
    * fix: make sure default values can be the number zero
    
    * refactor: rename times to installments to match subscription model property
    
    * feature: update donation summary template to support donation type
    
    * refactor: use custom rules for specific donation amount validation
    
    * feature: add exception for missing form
    
    * refactor: remove extra formId
    
    * refactor: make amount input controlled with custom amount modifier
    
    * wip: update amount field styles
    
    * wip: hook up the amount field functionality to the front-end
    
    * docs: update todo notes
    
    * refactor: use field label for donation amount
    
    * refactor: minor style updates to amount field
    
    * refactor: minor form style updates
    
    * wip: sanitize custom amount field on change
    
    * wip: test out currency input component
    
    * refactor: co-locate all the sub components
    
    * refactor: cleanup unused and elevate level select callback
    
    * refactor: add custom ref to custom amount input for resetting value
    
    * feature: add useCurrencyFormatter to window hooks
    
    * refactor: format custom input with currency symbol and center align
    
    * fix: clear the internal custom amount input value when ref value is updated
    
    * fix: amoutn min and max validation
    
    * refactor: change fixed message
    
    * chore: remove console
    
    * docs: update since tags
    
    * tests: update tests
    
    * docs: update docblock
    
    * refactor: add currency property
    
    * refactor: cleanup unused amount attributes
    
    * refactor: make block model attributes protectd and make use of getters/setters for attributes
    
    * feature: extend size rule and implement when set price and no custom amount
    
    * fix: fixed amount value type
    
    * feature: update amount min, max, and size rules with local versions
    
    ---------
    
    Co-authored-by: Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
    Co-authored-by: Jon Waldstein <jonwaldstein@Jons-MacBook-Air.local>

--- src/NextGen/DonationForm/resources/registrars/templates/elements/DonationSummary.tsx
+++ src/NextGen/DonationForm/resources/registrars/templates/elements/DonationSummary.tsx
@@ -1,26 +1,23 @@
-import {useMemo} from 'react';
 import {__} from '@wordpress/i18n';
+import useCurrencyFormatter from "@givewp/forms/app/hooks/useCurrencyFormatter";
 
 export default function DonationSummary() {
     const {useWatch} = window.givewp.form.hooks;
     const currency = useWatch({name: 'currency'});
     const amount = useWatch({name: 'amount'});
-    const formatter = useMemo(
-        () =>
-            new Intl.NumberFormat(navigator.language, {
-                style: 'currency',
-                currency: currency,
-            }),
-        [currency, navigator.language]
-    );
+    const donationType = useWatch({name: 'donationType'});
+    const period = useWatch({name: 'period'});
+    const givingFrequency = donationType !== 'subscription' ? __('One time', 'give') : `${period}ly`;
+    const formatter = useCurrencyFormatter(currency, {});
 
     return (
         <>
-            <strong>Donation Summary</strong>
+            {/*{TODO: make the labels dynamic}*/}
+            <strong>{__('Donation Summary', 'give')}</strong>
             <ul className="givewp-elements-donationSummary__list">
-                <LineItem label={__('Payment Amount', 'give')} value={formatter.format(Number(amount))} />
-                <LineItem label={__('Giving Frequency', 'give')} value={__('One time', 'give')} />
-                <LineItem label={__('Donation Total', 'give')} value={formatter.format(Number(amount))} />
+                <LineItem label={__('Payment Amount', 'give')} value={formatter.format(Number(amount))}/>
+                <LineItem label={__('Giving Frequency', 'give')} value={givingFrequency}/>
+                <LineItem label={__('Donation Total', 'give')} value={formatter.format(Number(amount))}/>
             </ul>
         </>
     );
