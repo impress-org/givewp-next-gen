d1dbd2dfb2698a5fce3885c3b68ee14bbf46da20
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2023-02-10 11:04:54 -0500

    feature: add donation summary in default form schema and tweak styles

--- src/NextGen/DonationForm/resources/registrars/templates/elements/DonationSummary.tsx
+++ src/NextGen/DonationForm/resources/registrars/templates/elements/DonationSummary.tsx
@@ -1,6 +1,9 @@
 import {__} from '@wordpress/i18n';
 import useCurrencyFormatter from "@givewp/forms/app/hooks/useCurrencyFormatter";
 
+/**
+ * @since 0.1.0
+ */
 export default function DonationSummary() {
     const {useWatch} = window.givewp.form.hooks;
     const currency = useWatch({name: 'currency'});
@@ -11,19 +14,18 @@ export default function DonationSummary() {
     const formatter = useCurrencyFormatter(currency, {});
 
     return (
-        <>
-            {/*{TODO: make the labels dynamic}*/}
-            <strong>{__('Donation Summary', 'give')}</strong>
-            <ul className="givewp-elements-donationSummary__list">
-                <LineItem label={__('Payment Amount', 'give')} value={formatter.format(Number(amount))}/>
-                <LineItem label={__('Giving Frequency', 'give')} value={givingFrequency}/>
-                <LineItem label={__('Donation Total', 'give')} value={formatter.format(Number(amount))}/>
-            </ul>
-        </>
+        <ul className="givewp-elements-donationSummary__list">
+            <LineItem label={__('Payment Amount', 'give')} value={formatter.format(Number(amount))}/>
+            <LineItem label={__('Giving Frequency', 'give')} value={givingFrequency}/>
+            <LineItem label={__('Donation Total', 'give')} value={formatter.format(Number(amount))}/>
+        </ul>
     );
 }
 
-const LineItem = ({label, value}: {label: string; value: string}) => {
+/**
+ * @since 0.1.0
+ */
+const LineItem = ({label, value}: { label: string; value: string }) => {
     return (
         <li className="givewp-elements-donationSummary__list-item">
             <div>{label}</div>
