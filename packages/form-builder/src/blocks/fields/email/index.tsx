761d6dd00f454de76a31d5ec5deba4f04d728ccb
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2023-02-15 21:46:15 -0500

    refactor: move field settings into seperate files and update descriptions

--- packages/form-builder/src/blocks/fields/email/index.tsx
+++ packages/form-builder/src/blocks/fields/email/index.tsx
@@ -1,39 +1,9 @@
-import {Icon} from '@wordpress/icons';
-import {__} from '@wordpress/i18n';
-import settings from '../settings';
+import settings from './settings';
 import {FieldBlock} from '@givewp/form-builder/types/block';
-import FieldSettings from '@givewp/form-builder/blocks/fields/settings/Edit';
 
 const email: FieldBlock = {
     name: 'custom-block-editor/email-field',
-    settings: {
-        ...settings,
-        title: __('Email', 'custom-block-editor'),
-        supports: {
-            multiple: false,
-        },
-        attributes: {
-            ...settings.attributes,
-            lock: {remove: true},
-            label: {
-                default: __('Email Address'),
-            },
-        },
-        icon: () => (
-            <Icon
-                icon={
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6ZM20 6L12 11L4 6H20ZM20 18H4V8L12 13L20 8V18Z"
-                            fill="#000C00"
-                        />
-                    </svg>
-                }
-            />
-        ),
-
-        edit: (props) => <FieldSettings showRequired={false} {...props} />,
-    },
+    settings,
 };
 
 export default email;
--- packages/form-builder/src/blocks/fields/payment-gateways/index.tsx
+++ packages/form-builder/src/blocks/fields/payment-gateways/index.tsx
@@ -1,23 +1,9 @@
-import {__} from '@wordpress/i18n';
-import settings from '../settings';
+import settings from './settings';
 import {FieldBlock} from '@givewp/form-builder/types';
-import Edit from './Edit';
-import Icon from './Icon';
 
 const paymentGateways: FieldBlock = {
     name: 'custom-block-editor/payment-gateways',
-    settings: {
-        ...settings,
-        title: __('Payment Gateways', 'custom-block-editor'),
-        supports: {
-            multiple: false,
-        },
-        attributes: {
-            lock: {remove: true},
-        },
-        edit: Edit,
-        icon: Icon,
-    },
+    settings,
 };
 
 export default paymentGateways;
