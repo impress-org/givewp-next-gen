850f2d314272c82e0eeb2ecf94dbc328c19a7e76
Kyle B. Johnson <kyle.johnson@hey.com>
2023-03-16 17:12:58 -0400

    wip

--- src/NextGen/Gateways/PayPalCommerce/payPalCommerceGateway.tsx
+++ src/NextGen/Gateways/PayPalCommerce/payPalCommerceGateway.tsx
@@ -7,6 +7,7 @@ import {
 } from "@paypal/react-paypal-js";
 import type {Gateway} from '@givewp/forms/types';
 import {__} from "@wordpress/i18n";
+import {debounce} from "react-ace/lib/editorOptions";
 
 let amount;
 let firstName;
@@ -43,6 +44,38 @@ const CUSTOM_FIELD_STYLE = { // @todo How do we ensure that this matches the for
     fontWeight: "500",
     lineHeight: "1.2"
 };
+
+const getFormData = () => {
+    const formData = new FormData();
+    formData.append('give-amount', amount);
+    formData.append('give_first', firstName);
+    formData.append('give_last', lastName);
+    formData.append('give_email', email);
+    formData.append('give-form-id', payPalDonationsSettings.donationFormId);
+    formData.append('give-form-hash', payPalDonationsSettings.donationFormNonce);
+    return formData;
+}
+
+const validateHostedFields = () => {
+    return Object.values(
+        hostedField.cardFields.getState().fields
+    ).some((field) => field.isValid)
+}
+
+const createOrderHandler = async (data, actions) => {
+
+    const response = await fetch(`${payPalDonationsSettings.ajaxUrl}?action=give_paypal_commerce_create_order`, {
+        method: 'POST',
+        body: getFormData(),
+    } );
+    const responseJson = await response.json();
+
+    if ( ! responseJson.success ) {
+        throw responseJson.data.error;
+    }
+
+    return payPalOrderId = responseJson.data.id
+}
 
 const Divider = ({label, style = {}}) => {
 
@@ -82,6 +115,78 @@ const HoistHostedFieldContext = () => {
     return <></>
 }
 
+const FormFieldsProvider = ({children}) => {
+    const {useWatch} = window.givewp.form.hooks;
+    amount = useWatch({name: 'amount'});
+    firstName = useWatch({name: 'firstName'});
+    lastName = useWatch({name: 'lastName'});
+    email = useWatch({name: 'email'});
+
+    return children;
+}
+
+const SmartButtonsContainer = () => {
+    return (
+        <PayPalButtons
+            style={buttonsStyle}
+            // @ts-ignore
+            forceReRender={debounce(() => [amount, firstName, lastName, email], 500)}
+            createOrder={createOrderHandler}
+            onApprove={async (data, actions) => {
+                return actions.order.capture().then((details) => {
+                    // @ts-ignore
+                    document.forms[0].querySelector('[type="submit"]').click()
+                });
+            }}
+        />
+    )
+}
+
+const HostedFieldsContainer = () => {
+    return (
+        <PayPalHostedFieldsProvider
+            notEligibleError={<div>Your account is not eligible</div>}
+            createOrder={createOrderHandler}
+        >
+
+            <Divider label={__('Or pay with card', 'give')} style={{padding: '0 0 30px 0'}} />
+
+            <PayPalHostedField
+                id="card-number"
+                className="card-field"
+                style={CUSTOM_FIELD_STYLE}
+                hostedFieldType="number"
+                options={{
+                    selector: "#card-number",
+                    placeholder: "4111 1111 1111 1111",
+                }}
+            />
+            <PayPalHostedField
+                id="cvv"
+                className="card-field"
+                style={CUSTOM_FIELD_STYLE}
+                hostedFieldType="cvv"
+                options={{
+                    selector: "#cvv",
+                    placeholder: "123",
+                    maskInput: true,
+                }}
+            />
+            <PayPalHostedField
+                id="expiration-date"
+                className="givewp-fields"
+                style={CUSTOM_FIELD_STYLE}
+                hostedFieldType="expirationDate"
+                options={{
+                    selector: "#expiration-date",
+                    placeholder: "MM/YYYY",
+                }}
+            />
+            <HoistHostedFieldContext />
+        </PayPalHostedFieldsProvider>
+    )
+}
+
 const payPalCommerceGateway: Gateway = {
     id: 'paypal-commerce',
     supportsRecurring: false,
@@ -99,129 +204,32 @@ const payPalCommerceGateway: Gateway = {
             };
         }
 
-        const isHostedFieldsValid = Object.values(hostedField.cardFields.getState().fields)
-            .some((field) => field.isValid)
-
-        if(isHostedFieldsValid) {
-            return hostedField.cardFields
-                .submit({cardholderName: 'Tester T. Test',})
-                .then(async (data) => {
-
-                    let formData = new FormData();
-                    formData.append('give-amount', amount);
-                    formData.append('give_first', firstName);
-                    formData.append('give_last', lastName);
-                    formData.append('give_email', email);
-                    formData.append('give-form-id', payPalDonationsSettings.donationFormId);
-                    formData.append('give-form-hash', payPalDonationsSettings.donationFormNonce);
-
-                    await fetch( `${payPalDonationsSettings.ajaxUrl}?action=give_paypal_commerce_approve_order&order=` + data.orderId, {
-                        method: 'POST',
-                        body: formData,
-                    } );
-
-                    return {
-                        ...data,
-                        payPalOrderId: data.orderId,
-                    }
-                })
-                .catch((err) => {
-                    console.log('paypal commerce error', err)
-                    return;
-                });
-        } else {
-            alert('Invalid hosted fields')
+        if(!validateHostedFields()) {
+            throw new Error('Invalid hosted fields');
         }
 
+        return hostedField.cardFields
+            .submit({cardholderName: 'Tester T. Test',})
+            .then(async (data) => {
+                await fetch( `${payPalDonationsSettings.ajaxUrl}?action=give_paypal_commerce_approve_order&order=` + data.orderId, {
+                    method: 'POST',
+                    body: getFormData(),
+                } );
+                return {...data, payPalOrderId: data.orderId}
+            })
+            .catch((err) => {
+                console.log('paypal commerce error', err)
+                throw new Error('paypal commerce error');
+            });
     },
     Fields() { // Can we get this.settings to be available here?
-        const {useWatch} = window.givewp.form.hooks;
-        amount = useWatch({name: 'amount'});
-        firstName = useWatch({name: 'firstName'});
-        lastName = useWatch({name: 'lastName'});
-        email = useWatch({name: 'email'});
-
-        const createOrderHandler = async (data, actions) => {
-
-            let formData = new FormData();
-            formData.append('give-amount', amount);
-            formData.append('give_first', firstName);
-            formData.append('give_last', lastName);
-            formData.append('give_email', email);
-            formData.append('give-form-id', payPalDonationsSettings.donationFormId);
-            formData.append('give-form-hash', payPalDonationsSettings.donationFormNonce);
-
-            // eslint-disable-next-line
-            const response = await fetch(`${payPalDonationsSettings.ajaxUrl}?action=give_paypal_commerce_create_order`, {
-                method: 'POST',
-                body: formData,
-            } );
-            const responseJson = await response.json();
-
-            if ( ! responseJson.success ) {
-                throw responseJson.data.error;
-            }
-
-            return payPalOrderId = responseJson.data.id
-        }
-
         return (
-            <fieldset className="no-fields">
+            <FormFieldsProvider>
                 <PayPalScriptProvider options={payPalDonationsSettings.sdkOptions}>
-                    <PayPalButtons
-                        style={buttonsStyle}
-                        forceReRender={[amount, firstName, lastName, email]}
-                        createOrder={createOrderHandler}
-                        onApprove={async (data, actions) => {
-                            return actions.order.capture().then((details) => {
-                                // @ts-ignore
-                                document.forms[0].querySelector('[type="submit"]').click()
-                            });
-                        }}
-                    />
-
-                    <PayPalHostedFieldsProvider
-                        notEligibleError={<div>Your account is not eligible</div>}
-                        createOrder={createOrderHandler}
-                        >
-
-                        <Divider label={__('Or pay with card', 'give')} style={{padding: '0 0 30px 0'}} />
-
-                        <PayPalHostedField
-                            id="card-number"
-                            className="card-field"
-                            style={CUSTOM_FIELD_STYLE}
-                            hostedFieldType="number"
-                            options={{
-                                selector: "#card-number",
-                                placeholder: "4111 1111 1111 1111",
-                            }}
-                        />
-                        <PayPalHostedField
-                            id="cvv"
-                            className="card-field"
-                            style={CUSTOM_FIELD_STYLE}
-                            hostedFieldType="cvv"
-                            options={{
-                                selector: "#cvv",
-                                placeholder: "123",
-                                maskInput: true,
-                            }}
-                        />
-                        <PayPalHostedField
-                            id="expiration-date"
-                            className="givewp-fields"
-                            style={CUSTOM_FIELD_STYLE}
-                            hostedFieldType="expirationDate"
-                            options={{
-                                selector: "#expiration-date",
-                                placeholder: "MM/YYYY",
-                            }}
-                        />
-                        <HoistHostedFieldContext />
-                    </PayPalHostedFieldsProvider>
+                    <SmartButtonsContainer />
+                    <HostedFieldsContainer />
                 </PayPalScriptProvider>
-            </fieldset>
+            </FormFieldsProvider>
         );
     },
 };
