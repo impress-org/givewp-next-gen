0be195a1562371ed8faea6aee2c41266f791a9da
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-04-25 10:23:52 -0400

    feature: add initial MultiStepForm component

---
+++ src/NextGen/DonationForm/resources/app/form/MultiStepForm.tsx
@@ -0,0 +1,90 @@
+import {FormProvider, useForm, useFormState} from 'react-hook-form';
+import {joiResolver} from '@hookform/resolvers/joi';
+
+import getWindowData from '../utilities/getWindowData';
+import {useGiveDonationFormStore} from '../store';
+import {Section} from '@givewp/forms/types';
+import {withTemplateWrapper} from '../templates';
+import {useCallback} from 'react';
+import SectionNode from '../fields/SectionNode';
+import {ObjectSchema} from 'joi';
+import DonationFormErrorBoundary from '@givewp/forms/app/errors/boundaries/DonationFormErrorBoundary';
+import handleSubmitRequest from '@givewp/forms/app/utilities/handleFormSubmitRequest';
+
+const {donateUrl, inlineRedirectRoutes} = getWindowData();
+const formTemplates = window.givewp.form.templates;
+
+const FormTemplate = withTemplateWrapper(formTemplates.layouts.form);
+const FormSectionTemplate = withTemplateWrapper(formTemplates.layouts.section, 'section');
+
+export default function MultiStepForm({defaultValues, sections, validationSchema}: PropTypes) {
+    const {gateways} = useGiveDonationFormStore();
+
+    const getGateway = useCallback((gatewayId) => gateways.find(({id}) => id === gatewayId), []);
+
+    const methods = useForm<FormInputs>({
+        defaultValues,
+        resolver: joiResolver(validationSchema),
+        reValidateMode: 'onBlur',
+    });
+
+    const {handleSubmit, setError, control} = methods;
+
+    const {errors, isSubmitting, isSubmitSuccessful} = useFormState({control});
+
+    const formError = errors.hasOwnProperty('FORM_ERROR') ? errors.FORM_ERROR.message : null;
+
+    return (
+        <FormProvider {...methods}>
+            <DonationFormErrorBoundary>
+                <FormTemplate
+                    formProps={{
+                        id: 'give-next-gen',
+                        onSubmit: handleSubmit((values) =>
+                            handleSubmitRequest(
+                                values,
+                                setError,
+                                getGateway(values.gatewayId),
+                                donateUrl,
+                                inlineRedirectRoutes
+                            )
+                        ),
+                    }}
+                    isSubmitting={isSubmitting || isSubmitSuccessful}
+                    formError={formError}
+                >
+                    <>
+                        {sections.map((section) => {
+                            return (
+                                <DonationFormErrorBoundary key={section.name}>
+                                    <FormSectionTemplate key={section.name} section={section}>
+                                        {section.nodes.map((node) => (
+                                            <DonationFormErrorBoundary key={node.name}>
+                                                <SectionNode key={node.name} node={node} />
+                                            </DonationFormErrorBoundary>
+                                        ))}
+                                    </FormSectionTemplate>
+                                </DonationFormErrorBoundary>
+                            );
+                        })}
+                    </>
+                </FormTemplate>
+            </DonationFormErrorBoundary>
+        </FormProvider>
+    );
+}
+
+type PropTypes = {
+    sections: Section[];
+    defaultValues: object;
+    validationSchema: ObjectSchema;
+};
+
+type FormInputs = {
+    FORM_ERROR: string;
+    amount: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gatewayId: string;
+};
