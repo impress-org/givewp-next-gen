1cc5f732fec33508acb14e5c71fe1e163df9c1b6
Paulo Iankoski <me@wpdev.com.br>
2023-04-26 23:25:35 -0300

    feature: add support to have specific files for form builder or form view

--- src/NextGen/Framework/FormExtensions/FormExtension.php
+++ src/NextGen/Framework/FormExtensions/FormExtension.php
@@ -7,32 +7,32 @@ use Give\NextGen\Framework\FormExtensions\Contracts\FormExtensionInterface;
 /**
  * The FormExtension is meant to be extended to create custom GiveWP form extensions.
  *
- * @unreleased 
+ * @unreleased
  */
 abstract class FormExtension implements FormExtensionInterface
 {
     /**
      * The unique identifier of the extension
      *
-     * @unreleased 
+     * @unreleased
      */
     abstract public static function id(): string;
 
     /**
      * THe human-readable name of the extension
      *
-     * @unreleased 
+     * @unreleased
      */
     abstract public static function name(): string;
 
     /**
      * Full URL of the stylesheet, or path of the stylesheet relative to the WordPress root directory.
      *
-     * @unreleased 
+     * @unreleased
      *
      * @return string|false
      */
-    public function css()
+    public function formBuilderCss()
     {
         return false;
     }
@@ -40,11 +40,11 @@ abstract class FormExtension implements FormExtensionInterface
     /**
      * Full URL of the script, or path of the script relative to the WordPress root directory.
      *
-     * @unreleased 
+     * @unreleased
      *
      * @return string|false
      */
-    public function js()
+    public function formBuilderJs()
     {
         return false;
     }
@@ -57,7 +57,44 @@ abstract class FormExtension implements FormExtensionInterface
      *
      * @return array
      */
-    public function dependencies(): array
+    public function formBuilderDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * Full URL of the stylesheet, or path of the stylesheet relative to the WordPress root directory.
+     *
+     * @unreleased
+     *
+     * @return string|false
+     */
+    public function formViewCss()
+    {
+        return false;
+    }
+
+    /**
+     * Full URL of the script, or path of the script relative to the WordPress root directory.
+     *
+     * @unreleased
+     *
+     * @return string|false
+     */
+    public function formViewJs()
+    {
+        return false;
+    }
+
+    /**
+     * An array of dependencies compatible with the `$deps` parameter in wp_enqueue_script
+     *
+     * @see https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+     * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-dependency-extraction-webpack-plugin/#wordpress
+     *
+     * @return array
+     */
+    public function formViewDependencies(): array
     {
         return [];
     }
