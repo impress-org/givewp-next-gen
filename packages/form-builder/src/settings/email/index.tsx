2d3e6fad6e4768dd19ff96b784c5aceb191bb7c4
Kyle B. Johnson <kyle.johnson@hey.com>
2023-05-01 14:53:05 -0400

    wip: Conditionally show email inspector settings

--- packages/form-builder/src/settings/email/index.tsx
+++ packages/form-builder/src/settings/email/index.tsx
@@ -29,43 +29,47 @@ export default () => {
                     onChange={(emailOptionsStatus) => dispatch(setFormSettings({emailOptionsStatus}))}
                     />
             </PanelRow>
-            <PanelRow>
-                <SelectControl
-                    label={__('Email Template', 'givewp')}
-                    help={__('Choose your template from the available registered template types', 'givewp')}
-                    options={[
-                        {label: __('Default template', 'givewp'), value: 'default'},
-                        {label: __('No template, plain text only', 'givewp'), value: 'none'},
-                    ]}
-                    value={emailTemplate}
-                    onChange={(emailTemplate) => dispatch(setFormSettings({emailTemplate}))}
-                />
-            </PanelRow>
-            <PanelRow>
-                <LogoUpload
-                    value={emailLogo}
-                    onChange={(emailLogo) => dispatch(setFormSettings({emailLogo}))}
-                />
-            </PanelRow>
-            <PanelRow>
-                <TextControl
-                    label={__('From Name', 'givewp')}
-                    help={__('The name which appears in the "From" field in all GiveWP donation emails.', 'givewp')}
-                    value={emailFromName}
-                    onChange={(emailFromName) => dispatch(setFormSettings({emailFromName}))}
-                />
-            </PanelRow>
-            <PanelRow>
-                <TextControl
-                    label={__('From Email', 'givewp')}
-                    help={__('Email address from which all GiveWP emails are sent from. This will act as the "from" and "reply-to" email address.', 'givewp')}
-                    value={emailFromEmail}
-                    onChange={(emailFromEmail) => dispatch(setFormSettings({emailFromEmail}))}
-                />
-            </PanelRow>
-            <PanelRow>
-                <EmailTemplateOptions />
-            </PanelRow>
+            {'enabled' === emailOptionsStatus && (
+                <>
+                    <PanelRow>
+                        <SelectControl
+                            label={__('Email Template', 'givewp')}
+                            help={__('Choose your template from the available registered template types', 'givewp')}
+                            options={[
+                                {label: __('Default template', 'givewp'), value: 'default'},
+                                {label: __('No template, plain text only', 'givewp'), value: 'none'},
+                            ]}
+                            value={emailTemplate}
+                            onChange={(emailTemplate) => dispatch(setFormSettings({emailTemplate}))}
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <LogoUpload
+                            value={emailLogo}
+                            onChange={(emailLogo) => dispatch(setFormSettings({emailLogo}))}
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <TextControl
+                            label={__('From Name', 'givewp')}
+                            help={__('The name which appears in the "From" field in all GiveWP donation emails.', 'givewp')}
+                            value={emailFromName}
+                            onChange={(emailFromName) => dispatch(setFormSettings({emailFromName}))}
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <TextControl
+                            label={__('From Email', 'givewp')}
+                            help={__('Email address from which all GiveWP emails are sent from. This will act as the "from" and "reply-to" email address.', 'givewp')}
+                            value={emailFromEmail}
+                            onChange={(emailFromEmail) => dispatch(setFormSettings({emailFromEmail}))}
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <EmailTemplateOptions />
+                    </PanelRow>
+                </>
+            )}
         </PanelBody>
     </>
 }
