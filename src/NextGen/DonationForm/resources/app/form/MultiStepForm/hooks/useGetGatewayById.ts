46c1100a12d2b02342fa9e371e11efbfb6b88262
Jon Waldstein <jonwaldstein@Jons-MacBook-Air.local>
2023-05-05 10:22:00 -0400

    docs: update since tag

--- src/NextGen/DonationForm/resources/app/form/MultiStepForm/hooks/useGetGatewayById.ts
+++ src/NextGen/DonationForm/resources/app/form/MultiStepForm/hooks/useGetGatewayById.ts
@@ -1,8 +1,11 @@
 import {useCallback} from 'react';
 import {useDonationFormState} from '@givewp/forms/app/store';
 
+/**
+ * @unreleased
+ */
 export default function useGetGatewayById() {
     const {gateways} = useDonationFormState();
 
-    return useCallback((gatewayId: string) => gateways?.find(({id}) => id === gatewayId), []);
+    return useCallback((gatewayId: string) => gateways?.find(({id}) => id === gatewayId), [gateways]);
 }
