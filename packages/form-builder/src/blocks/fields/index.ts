b94e352abbd90adef38d355140081b1c275d29b7
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-05-18 15:28:19 -0400

    chore: remove test field

--- packages/form-builder/src/blocks/fields/index.ts
+++ packages/form-builder/src/blocks/fields/index.ts
@@ -6,13 +6,12 @@ import paymentGateways from './payment-gateways';
 import donationSummary from './donation-summary';
 import amount from './amount';
 import {FieldBlock} from '@givewp/form-builder/types';
-import fee from '@givewp/form-builder/blocks/fields/fee';
 
 /**
  * @note Blocks in the appender are listed in the order that the blocks are registered.
  */
 
-const fieldBlocks: FieldBlock[] = [text, company, donorName, email, paymentGateways, donationSummary, amount, fee];
+const fieldBlocks: FieldBlock[] = [text, company, donorName, email, paymentGateways, donationSummary, amount];
 
 
 export default fieldBlocks;
