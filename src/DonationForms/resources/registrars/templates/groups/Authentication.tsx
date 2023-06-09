9ab5d64321450104cf3ce6e1811f65c41a69f18b
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-06-09 14:34:14 -0400

    feature: add login page redirect

--- src/DonationForms/resources/registrars/templates/groups/Authentication.tsx
+++ src/DonationForms/resources/registrars/templates/groups/Authentication.tsx
@@ -1,9 +1,38 @@
-import {FC, useState} from "react";
-import {__} from "@wordpress/i18n";
+import {FC, useState} from 'react';
+import {__} from '@wordpress/i18n';
 import type {FieldProps} from '@givewp/forms/propTypes';
-import getWindowData from "@givewp/forms/app/utilities/getWindowData";
-import postData from "@givewp/forms/app/utilities/postData";
-import {GroupProps} from "@givewp/forms/propTypes";
+import {GroupProps} from '@givewp/forms/propTypes';
+import getWindowData from '@givewp/forms/app/utilities/getWindowData';
+import postData from '@givewp/forms/app/utilities/postData';
+import getCurrentFormUrlData from '@givewp/forms/app/utilities/getCurrentFormUrlData';
+import {useCallback} from '@wordpress/element';
+
+const {originUrl, isEmbed, embedId} = getCurrentFormUrlData();
+
+const handleLoginPageRedirected = () => {
+    const formPageUrl = new URL(window.top.location.href);
+
+    const isAuthRedirect =
+        formPageUrl.searchParams.has('givewp-auth-redirect') &&
+        formPageUrl.searchParams.get('givewp-auth-redirect') === 'true';
+
+    if (isAuthRedirect) {
+        const isEmbedRedirect =
+            formPageUrl.searchParams.has('givewp-is-embed') &&
+            formPageUrl.searchParams.get('givewp-is-embed') === 'true';
+        const isThisEmbed =
+            formPageUrl.searchParams.has('givewp-embed-id') &&
+            formPageUrl.searchParams.get('givewp-embed-id') === embedId;
+
+        if (isEmbedRedirect && isThisEmbed) {
+            window.frameElement.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    }
+};
+
+handleLoginPageRedirected();
 
 interface AuthProps extends GroupProps {
     fields: {
@@ -12,20 +41,41 @@ interface AuthProps extends GroupProps {
     };
     required: boolean;
     isAuthenticated: boolean;
-    loginRedirect: boolean
+    loginRedirect: boolean;
     loginRedirectUrl: string;
-    loginNotice:  string;
+    loginNotice: string;
     loginConfirmation: string;
 }
 
 export default function Authentication({
-   fields: {login: Login, password: Password},
-   required, isAuthenticated, loginRedirect, loginRedirectUrl, loginNotice, loginConfirmation,
+    fields: {login: Login, password: Password},
+    required,
+    isAuthenticated,
+    loginRedirect,
+    loginRedirectUrl,
+    loginNotice,
+    loginConfirmation,
 }: AuthProps) {
-
     const [isAuth, setIsAuth] = useState<boolean>(isAuthenticated);
     const [showLogin, setShowLogin] = useState<boolean>(required);
     const toggleShowLogin = () => setShowLogin(!showLogin);
+
+    const handleLoginPageRedirect = useCallback(() => {
+        const loginUrl = new URL(loginRedirectUrl);
+        const redirectUrl = new URL(originUrl);
+
+        redirectUrl.searchParams.append('givewp-auth-redirect', 'true');
+
+        if (isEmbed) {
+            redirectUrl.searchParams.append('givewp-is-embed', String(isEmbed));
+            redirectUrl.searchParams.append('givewp-embed-id', embedId);
+        }
+
+        // use wp core login redirect param
+        loginUrl.searchParams.set('redirect_to', redirectUrl.toString());
+
+        window.top.location.assign(loginUrl);
+    }, [loginRedirectUrl, isEmbed, embedId]);
 
     return (
         <>
@@ -35,20 +85,18 @@ export default function Authentication({
                 </p>
             )}
             {!isAuth && showLogin && (
-                <LoginForm
-                    success={() => setIsAuth(true)}
-                    fail={(errorMessage) => alert(errorMessage)}
-                >
+                <LoginForm success={() => setIsAuth(true)} fail={(errorMessage) => alert(errorMessage)}>
                     <Login />
                     <Password />
                 </LoginForm>
             )}
             {!isAuth && !showLogin && (
                 <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-                    {loginRedirect
-                        ? <a href={loginRedirectUrl}>{loginNotice}</a>
-                        : <LoginNotice onClick={toggleShowLogin}>{loginNotice}</LoginNotice>
-                    }
+                    {loginRedirect ? (
+                        <button onClick={handleLoginPageRedirect}>{loginNotice}</button>
+                    ) : (
+                        <LoginNotice onClick={toggleShowLogin}>{loginNotice}</LoginNotice>
+                    )}
                 </div>
             )}
         </>
@@ -56,7 +104,6 @@ export default function Authentication({
 }
 
 const LoginForm = ({children, success, fail}) => {
-
     const {authUrl} = getWindowData();
     const {useWatch, useFormContext} = window.givewp.form.hooks;
     const {setValue, getValues} = useFormContext();
@@ -66,8 +113,7 @@ const LoginForm = ({children, success, fail}) => {
     const password = useWatch({name: 'password'});
 
     const tryLogin = async (event) => {
-
-        event.preventDefault()
+        event.preventDefault();
 
         const {response} = await postData(authUrl, {
             login: login,
@@ -75,35 +121,42 @@ const LoginForm = ({children, success, fail}) => {
         });
 
         const {data: responseData} = await response.json();
-        if(response.ok) {
+        if (response.ok) {
             setValue('firstName', firstName || responseData.firstName);
             setValue('lastName', lastName || responseData.lastName);
             setValue('email', email || responseData.email);
             success();
         } else {
-            console.log('here', responseData)
+            console.log('here', responseData);
             fail(
                 'authentication_error' === responseData.type
                     ? responseData.message
                     : __('Something went wrong. Please try or again or contact a site administrator.', 'givewp')
             );
         }
-    }
+    };
 
     return (
         <div style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
-            <div style={{display: 'flex', flexDirection: 'row', gap: '15px'}}>
-                {children}
-            </div>
+            <div style={{display: 'flex', flexDirection: 'row', gap: '15px'}}>{children}</div>
 
-            <div style={{display: 'flex', flexDirection: 'row-reverse', gap: '15px', justifyContent: 'space-between', alignItems: 'baseline'}}>
-                <button style={{width: 'auto'}} onClick={tryLogin}>{__('Log In', 'givewp')}</button>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row-reverse',
+                    gap: '15px',
+                    justifyContent: 'space-between',
+                    alignItems: 'baseline',
+                }}
+            >
+                <button style={{width: 'auto'}} onClick={tryLogin}>
+                    {__('Log In', 'givewp')}
+                </button>
                 <a href={'#'}>{__('Reset Password', 'givewp')}</a>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
 const LoginNotice = ({children, onClick}) => {
     return (
