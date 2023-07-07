04924c47fdeb44c9fbc71a3df43ebd00259f069f
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-07 12:30:30 -0400

    wip

--- src/FormMigration/ServiceProvider.php
+++ src/FormMigration/ServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Give\FormMigration;
 
-use Give\FormBuilder\Routes\RegisterFormBuilderRestRoutes;
 use Give\FormMigration\Commands\MigrationCommand;
-use Give\Helpers\Hooks;
+use Give\FormMigration\Controllers\MigrationController;
 use Give\ServiceProviders\ServiceProvider as ServiceProviderInterface;
 use WP_CLI;
+use WP_REST_Server;
 
 /**
  * @since 0.1.0
@@ -34,10 +34,32 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function boot()
     {
-        add_action('rest_api_init', function() {
-            include __DIR__ . '/routes.php';
-        });
+        $this->registerRoutes();
+        $this->registerCommands();
+    }
 
+    protected function registerRoutes()
+    {
+        add_action('rest_api_init', function() {
+            register_rest_route('givewp/v3', '/migrate/(?P<id>\d+)', [
+                'methods' => WP_REST_Server::CREATABLE,
+                'callback' => new MigrationController(),
+                'permission_callback' => function () {
+                    return current_user_can('manage_options');
+                },
+                'args' => [
+                    'id' => [
+                        'type' => 'integer',
+                        'sanitize_callback' => 'absint',
+                        'description' => __('The ID of the form (v2) to migrate to v3.', 'givewp'),
+                    ],
+                ],
+            ]);
+        });
+    }
+
+    protected function registerCommands()
+    {
         if ( defined( 'WP_CLI' ) && WP_CLI ) {
             error_reporting( E_ALL & ~E_DEPRECATED );
             WP_CLI::add_command('givewp form:migrate', MigrationCommand::class);
