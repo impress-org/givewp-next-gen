1b80e134051d2b6b755665a4baaf237553b7c6ae
Glauber Silva <glauber.silva@givewp.com>
2023-07-11 16:03:39 -0300

    feature: add test mode notice

--- src/DonationForms/resources/registrars/templates/fields/Gateways.tsx
+++ src/DonationForms/resources/registrars/templates/fields/Gateways.tsx
@@ -20,6 +20,25 @@ function GatewayFieldsErrorFallback({error, resetErrorBoundary}) {
     );
 }
 
+const TestModeNotice = () => {
+    return (
+        <div className={'givewp-test-mode-notice'}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1ZM12 7C11.4477 7 11 7.44772 11 8C11 8.55228 11.4477 9 12 9H12.01C12.5623 9 13.01 8.55228 13.01 8C13.01 7.44772 12.5623 7 12.01 7H12ZM13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12V16C11 16.5523 11.4477 17 12 17C12.5523 17 13 16.5523 13 16V12Z"
+                    fill="#F29718"
+                />
+            </svg>{' '}
+            <p>
+                {__('Test mode is', 'give')} <strong>{__('enabled', 'give')}</strong>.{' '}
+                {__('While in test mode no live donations are processed.', 'give')}
+            </p>
+        </div>
+    );
+};
+
 export default function Gateways({isTestMode, inputProps, gateways}: GatewayFieldProps) {
     const {errors} = window.givewp.form.hooks.useFormState();
 
@@ -31,7 +50,7 @@ export default function Gateways({isTestMode, inputProps, gateways}: GatewayFiel
         <>
             {gateways.length > 0 ? (
                 <>
-                    {isTestMode && <div>NOTICE GOES HERE</div>}
+                    {isTestMode && <TestModeNotice />}
                     <ul style={{listStyleType: 'none', padding: 0}}>
                         {gateways.map((gateway, index) => (
                             <GatewayOption gateway={gateway} index={index} key={gateway.id} inputProps={inputProps} />
