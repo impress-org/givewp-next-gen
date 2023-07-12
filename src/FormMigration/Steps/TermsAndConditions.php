160ea9a3015e6c5c385ee8428d1820c59ec4f952
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-12 15:56:01 -0400

    wip: Update TermsAndConditions migration step

--- src/FormMigration/Steps/TermsAndConditions.php
+++ src/FormMigration/Steps/TermsAndConditions.php
@@ -3,13 +3,20 @@
 namespace Give\FormMigration\Steps;
 
 use Give\FormMigration\Contracts\FormMigrationStep;
+use Give\Framework\Blocks\BlockFactory;
 
 class TermsAndConditions extends FormMigrationStep
 {
+    public function canHandle(): bool
+    {
+        return $this->formV2->isTermsEnabled();
+    }
+
     public function process()
     {
-//        $this->formV2->isTermsEnabled();
-//        $this->formV2->getTermsAgreementLabel();
-//        $this->formV2->getTermsAgreementText();
+        $this->fieldBlocks->insertAfter('givewp/payment-gateways', BlockFactory::termsAndConditions([
+            'checkboxLabel' => $this->formV2->getTermsAgreementLabel(),
+            'agreementText' => $this->formV2->getTermsAgreementText(),
+        ]));
     }
 }
--- src/Framework/Blocks/BlockFactory.php
+++ src/Framework/Blocks/BlockFactory.php
@@ -34,4 +34,24 @@ class BlockFactory
             ],
         ]);
     }
+
+    public static function termsAndConditions(array $attributes)
+    {
+        return BlockModel::make([
+            'name' => 'givewp/terms-and-conditions',
+            'attributes' => array_merge([
+                'useGlobalSettings' => false,
+                'checkboxLabel' => __('I agree to the Terms and conditions.', 'give'),
+                'displayType' => 'showFormTerms',
+                'linkText' => __('Show terms', 'give'),
+                'linkUrl' => '',
+                'agreementText' => __(
+                    'Acceptance of any contribution, gift or grant is at the discretion of the GiveWP. The GiveWP will not accept any gift unless it can be used or expended consistently with the purpose and mission of the GiveWP. No irrevocable gift, whether outright or life-income in character, will be accepted if under any reasonable set of circumstances the gift would jeopardize the donor’s financial security.The GiveWP will refrain from providing advice about the tax or other treatment of gifts and will encourage donors to seek guidance from their own professional advisers to assist them in the process of making their donation.',
+                    'give'
+                ),
+                'modalHeading' => __('Do you consent to the following', 'give'),
+                'modalAcceptanceText' => __('Accept', 'give'),
+            ], $attributes),
+        ]);
+    }
 }
