938b2038f198a6d42a7ed59ba0e0ef9380018e25
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-07-12 12:26:17 -0400

    feature: add currency/locale formatting to message

--- src/DonationForms/resources/registrars/templates/groups/DonationAmount/DonationAmountCurrencySwitcherMessage.tsx
+++ src/DonationForms/resources/registrars/templates/groups/DonationAmount/DonationAmountCurrencySwitcherMessage.tsx
@@ -46,12 +46,24 @@ export default function DonationAmountCurrencySwitcherMessage({
     message,
 
 }: DonationAmountCurrencySwitcherMessageProps ) {
-    const {useWatch} = window.givewp.form.hooks;
+    const {useWatch, useCurrencyFormatter} = window.givewp.form.hooks;
     const currency = useWatch({name: 'currency'});
+    const newCurrencyFormatter = useCurrencyFormatter(currency);
 
     // TODO: add base currency identifier to currencySettings
     const baseCurrency = currencySettings.find((setting) => setting.exchangeRate === 0)?.id ?? 'USD';
-    const newCurrencyRate = currencySettings.find((setting) => setting.id === currency)?.exchangeRate.toFixed(2) ?? '1.00';
 
-    return <CurrencySwitcherMessage baseCurrency={baseCurrency} message={message} newCurrency={currency} newCurrencyRate={newCurrencyRate} />;
+    const baseCurrencyFormatter = useCurrencyFormatter(baseCurrency);
+
+    const newCurrencyRate =
+        currencySettings.find((setting) => setting.id === currency)?.exchangeRate.toFixed(2) ?? '1.00';
+
+    return (
+        <CurrencySwitcherMessage
+            baseCurrency={baseCurrency}
+            message={message.replace('1.00', baseCurrencyFormatter.format(1))}
+            newCurrency={currency}
+            newCurrencyRate={newCurrencyFormatter.format(Number(newCurrencyRate))}
+        />
+    );
 }
