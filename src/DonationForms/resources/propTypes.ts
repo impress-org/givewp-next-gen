4e364a8ea59720e31dbc526572c5720f6ee7cc75
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-07-12 16:52:30 -0400

    chore: make messages optiona

--- src/DonationForms/resources/propTypes.ts
+++ src/DonationForms/resources/propTypes.ts
@@ -105,7 +105,7 @@ export interface AmountProps extends FieldProps {
     fixedAmountValue: number;
     currencySettings?: CurrencySetting[];
     currencySwitcherMessage?: string;
-    messages: ReactNode;
+    messages?: ReactNode;
 }
 
 export interface ParagraphProps extends ElementProps {
