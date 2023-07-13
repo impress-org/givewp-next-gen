f0319661cdf687d35e136620c198b470bb1b3163
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-13 13:55:12 -0400

    wip

--- src/FormMigration/Steps/FormTitle.php
+++ src/FormMigration/Steps/FormTitle.php
@@ -8,6 +8,8 @@ class FormTitle extends FormMigrationStep
 {
     public function process()
     {
-        $this->formV3->title = sprintf('%s [v3]', $this->formV2->title);
+        $this->formV3->title
+            = $this->formV3->settings->formTitle
+            = sprintf('%s [v3]', $this->formV2->title);
     }
 }
