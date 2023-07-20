f8c95e154170db744bf7c37e78f907d92bfa8065
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-20 13:26:41 -0400

    wip: Add revenue table to transfer

--- src/FormMigration/Controllers/TransferController.php
+++ src/FormMigration/Controllers/TransferController.php
@@ -24,10 +24,19 @@ class TransferController
 
     public function __invoke(DonationForm $formV3)
     {
-        DB::table('give_donationmeta')
-            ->where('meta_key', '_give_payment_form_id')
-            ->where('meta_value', give_get_meta($formV3->id, 'migratedFormId', true))
-            ->update(['meta_value' => $formV3->id]);
+        DB::transaction(function() use ($formV3) {
+
+            $formV2Id = give_get_meta($formV3->id, 'migratedFormId', true);
+
+            DB::table('give_donationmeta')
+                ->where('meta_key', '_give_payment_form_id')
+                ->where('meta_value', $formV2Id)
+                ->update(['meta_value' => $formV3->id]);
+
+            DB::table('give_revenue')
+                ->where('form_id', $formV2Id)
+                ->update(['meta_value' => $formV3->id]);
+        });
 
         return new WP_REST_Response(array('errors' => [], 'successes' => [
             $formV3->id
