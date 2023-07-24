28e8c288dd60819ffb8dd7ac5ec77768660f61c8
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-24 16:40:45 -0400

    wip

--- src/FormMigration/functions.php
+++ src/FormMigration/functions.php
@@ -6,16 +6,20 @@ use Give\Framework\Database\DB;
  * This function is used to "redirect" shortcodes and blocks
  * to a migrated form ID, if one exists.
  *
+ * ex: givewp_migrated_form_id($formId);
+ * ex: givewp_migrated_form_id($formId, $atts['id']);
+ *
  * @unreleased
  *
- * @param $formId $formId is used as an "output argument", meaning it is updated without needing to be returned.
+ * @param $formId int $formId is used as an "output argument", meaning it is updated without needing to be returned.
+ * @param $extraReference int[] Any additional references to update with the migrated form ID.
  *
  * @return void Note: $formId is an "output argument" - not a return value.
  */
-function givewp_migrated_form_id(&$formId) {
+function givewp_migrated_form_id(&$formId, &...$extraReference) {
     global $wpdb;
 
-    $formId = DB::get_var(
+    $formId = absint(DB::get_var(
         DB::prepare(
             "
                     SELECT `form_id`
@@ -24,5 +28,9 @@ function givewp_migrated_form_id(&$formId) {
                       AND `meta_value` = %d",
             $formId
         )
-    ) ?: $formId;
+    ) ) ?: $formId;
+
+    foreach($extraReference as &$reference) {
+        $reference = $formId;
+    }
 }
