6891731feed4c095be183720fedd49397a32c479
Glauber Silva <glauber.silva@givewp.com>
2023-07-26 13:13:36 -0300

    refactor: popover scroll improvements

--- src/FormBuilder/resources/js/form-builder/src/common/popover.js
+++ src/FormBuilder/resources/js/form-builder/src/common/popover.js
@@ -5,12 +5,19 @@ export default function popover() {
         if (!!popover) {
             const {top} = popover.getBoundingClientRect();
 
-            if (top < 130) {
-                const styleTop = Math.sign(event.deltaY) + 1;
-                popover.style.top = styleTop + 'rem';
+            if (top < 150) {
+                const topStyle = Math.sign(event.deltaY) + 1;
+                popover.style.top = topStyle + 'rem';
             } else {
                 popover.style.top = '0rem';
             }
+        }
+    });
+
+    window.addEventListener('click', (event) => {
+        const popover = document.querySelector('.components-popover.block-editor-block-popover');
+        if (!!popover) {
+            popover.style.top = '0rem';
         }
     });
 }
