3a4851c3050e019d471ce31db9d83ac854a97b87
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-28 14:54:38 -0400

    wip: Losen restriction

--- src/FormMigration/Actions/TransferDonations.php
+++ src/FormMigration/Actions/TransferDonations.php
@@ -10,21 +10,13 @@ class TransferDonations extends TransferAction
 {
     public function __invoke($destinationId)
     {
-        $updated = DB::table('give_donationmeta')
+        DB::table('give_donationmeta')
             ->where('meta_key', '_give_payment_form_id')
             ->where('meta_value', $this->sourceId)
             ->update(['meta_value' => $destinationId]);
 
-        if(!$updated) {
-            throw new Exception('Failed to transfer donations.');
-        }
-
-        $updated = DB::table('give_revenue')
+        DB::table('give_revenue')
             ->where('form_id', $this->sourceId)
             ->update(['form_id' => $destinationId]);
-
-        if(!$updated) {
-            throw new Exception('Failed to transfer donations.');
-        }
     }
 }
