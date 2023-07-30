04c19878c54e33cd1990b506fd0386f0b30548af
Paulo Iankoski <me@wpdev.com.br>
2023-07-30 16:39:44 -0300

    feature: replace custom state with RHF Controller wrapper

--- src/DonationForms/resources/registrars/templates/fields/MultiSelect.tsx
+++ src/DonationForms/resources/registrars/templates/fields/MultiSelect.tsx
@@ -1,5 +1,5 @@
 import Select from 'react-select';
-import {useState} from '@wordpress/element';
+import {Controller} from 'react-hook-form';
 
 import type {SelectFieldProps} from '@givewp/forms/propTypes';
 import styles from '../styles.module.scss';
@@ -15,15 +15,8 @@ export default function MultiSelect({
     inputProps,
 }: SelectFieldProps) {
     const {useFormContext} = window.givewp.form.hooks;
-    const {setValue} = useFormContext();
-    const [selected, setSelected] = useState(defaultValue);
+    const {control} = useFormContext();
     const FieldDescription = window.givewp.form.templates.layouts.fieldDescription;
-
-    // useEffect(() => {
-    //     if (selected) {
-    //         setValue(inputProps.name, selected);
-    //     }
-    // }, [selected]);
 
     return (
         <fieldset className={styles.multiSelectField}>
@@ -32,15 +25,33 @@ export default function MultiSelect({
                 {description && <FieldDescription description={description} />}
             </label>
             {fieldType === 'dropdown' ? (
-                <Select
-                    options={options}
-                    value={selected && options.filter(({value}) => selected.includes(value))}
-                    isMulti={true}
-                    isClearable={true}
-                    isSearchable={false}
-                    className="givewp-fields-multiSelect__input"
-                    classNamePrefix="givewp-fields-multiSelect"
-                    onChange={(selected) => setSelected(selected.map(({value}) => value))}
+                <Controller
+                    control={control}
+                    {...inputProps}
+                    render={({
+                        field: {onChange, onBlur, value, name, ref},
+                        fieldState: {invalid, isTouched, isDirty, error},
+                        formState,
+                    }) => (
+                        <Select
+                            ref={ref}
+                            options={options}
+                            defaultValue={
+                                value
+                                    ? options.filter(({value: optionValue}) =>
+                                          Object.values(value)?.includes(optionValue)
+                                      )
+                                    : null
+                            }
+                            isMulti={true}
+                            isClearable={true}
+                            isSearchable={false}
+                            className="givewp-fields-multiSelect__input"
+                            classNamePrefix="givewp-fields-multiSelect"
+                            onChange={(newValue) => onChange(newValue.map(({value}) => value))}
+                            aria-invalid={invalid ? 'true' : 'false'}
+                        />
+                    )}
                 />
             ) : (
                 <div className="givewp-fields-checkbox__options">
@@ -50,16 +61,7 @@ export default function MultiSelect({
                                 type="checkbox"
                                 {...inputProps}
                                 value={value}
-                                //checked={selected ? selected.includes(value) : false}
-                                // onChange={(event) => {
-                                //     const {value, checked} = event.target;
-                                //
-                                //     if (checked) {
-                                //         setSelected((prevSelected) => [...prevSelected, value]);
-                                //     } else {
-                                //         setSelected((prevSelected) => prevSelected.filter((item) => item !== value));
-                                //     }
-                                // }}
+                                checked={defaultValue && Object.values(defaultValue).includes(value)}
                             />
                             <label htmlFor={inputProps.name}>{label}</label>
                         </div>
