7a679bf61554d286b3c1bc81eeb2540b2e921e67
Glauber Silva <glauber.silva@givewp.com>
2023-08-01 13:23:07 -0300

    refactor: change skeleton classes to not cut off the popover element

--- src/FormBuilder/resources/js/form-builder/src/common/index.ts
+++ src/FormBuilder/resources/js/form-builder/src/common/index.ts
@@ -1,7 +1,3 @@
-import popover from '@givewp/form-builder/common/popover';
-
-popover();
-
 export {default as Storage} from './storage';
 export {default as slugify} from './slugify';
 export {default as getWindowData} from './getWindowData';
