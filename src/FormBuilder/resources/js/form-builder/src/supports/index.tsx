3ad4f89ecc521b905e1660a36c37a7d9cc504d4c
Paulo Iankoski <me@wpdev.com.br>
2023-08-06 22:11:03 -0300

    refactor: move conditional logic supports to FFM add-on

--- src/FormBuilder/resources/js/form-builder/src/supports/index.tsx
+++ src/FormBuilder/resources/js/form-builder/src/supports/index.tsx
@@ -1,7 +1,5 @@
-import registerConditionalLogicHooks from './conditional-logic';
 import registerFieldSettingsHooks from './field-settings';
 
 export default function registerHooks() {
-    registerConditionalLogicHooks();
     registerFieldSettingsHooks();
 }
