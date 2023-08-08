5acfbed86cf780566f1a0d51656ff489c7cf952a
Kyle B. Johnson <kyle.johnson@hey.com>
2023-08-08 14:48:22 -0400

    chore: Update tests

--- src/FormMigration/functions.php
+++ src/FormMigration/functions.php
@@ -11,7 +11,7 @@ use Give\Framework\Database\DB;
  *
  * @unreleased
  *
- * @param $formId int|int[] $formId is used as an "output argument", meaning it is updated without needing to be returned.
+ * @param $formId int $formId is used as an "output argument", meaning it is updated without needing to be returned.
  * @param $extraReference int[] Any additional references to update with the migrated form ID.
  *
  * @return void Note: $formId is an "output argument" - not a return value.
@@ -19,25 +19,19 @@ use Give\Framework\Database\DB;
 function give_redirect_form_id(&$formId, &...$extraReference) {
     global $wpdb;
 
-    if(is_array($formId)) {
-        foreach($formId as &$id) {
-            give_redirect_form_id($id);
-        }
-    } else {
-        $formId = absint(DB::get_var(
-            DB::prepare(
-                "
+    $formId = absint(DB::get_var(
+        DB::prepare(
+            "
                     SELECT `form_id`
                     FROM `{$wpdb->prefix}give_formmeta`
                     WHERE `meta_key` = 'redirectedFormId'
                       AND `meta_value` = %d",
-                $formId
-            )
-        ) ) ?: $formId;
+            $formId
+        )
+    ) ) ?: $formId;
 
-        foreach($extraReference as &$reference) {
-            $reference = $formId;
-        }
+    foreach($extraReference as &$reference) {
+        $reference = $formId;
     }
 }
 
